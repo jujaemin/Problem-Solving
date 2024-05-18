c34b76fc322f608aef9f8e8c5401f8bd54579baf
jujaemin <147119777+jujaemin@users.noreply.github.com>
2024-05-18 14:14:57 +0900

    [Silver I] Title: IOIOI, Time: 388 ms, Memory: 32368 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -4,47 +4,16 @@ N = int(sys.stdin.readline())
 M = int(sys.stdin.readline())
 S = sys.stdin.readline()
 pattern = ''
+cur, cnt, res = 0, 0, 0
 
-def KMP_table(pattern):
-    lp = len(pattern)
-    tb = [0 for _ in range(lp)]
-    
-    pidx = 0
-    for idx in range(1, lp):
-        while pidx > 0 and pattern[pidx] != pattern[idx]:
-            pidx = tb[pidx-1]
-
-        if pattern[idx] == pattern[pidx] :
-            pidx += 1
-            tb[idx] = pidx
-    
-    return tb
-
-def KMP(word, pattern):
-    table = KMP_table(pattern)
-    
-    results = [] 
-    pidx = 0
-    
-    for idx in range(len(word)):
-        while pidx > 0 and word[idx] != pattern[pidx] :
-            pidx = table[pidx-1]
-
-        if word[idx] == pattern[pidx]:
-            if pidx == len(pattern)-1 :
-                results.append(idx-len(pattern)+1)
-                pidx = table[pidx]
-            else:
-                pidx += 1
-    
-    return results
-
-for i in range(2*N+1):
-    if i%2 == 1:
-        pattern += 'O'
+while cur < (M-1):
+    if S[cur:cur+3] == 'IOI':
+        cnt += 1
+        cur += 2
+        if cnt == N:
+            res += 1
+            cnt -= 1
     else:
-        pattern += 'I'
-
-res = KMP(S, pattern)
-
-print(len(res))
+        cur += 1
+        cnt = 0
+print(res)
